b95580418f0826ec797c76ea1bbcc500699f85d4
hjpark <imcreator3@gmail.com>
2021-07-28 10:20:07 +0900

    3가지 메뉴 추천 로직 구현

--- src/parser/menu.ts
+++ src/parser/menu.ts
@@ -1,3 +1,4 @@
+import { parseHTML } from 'cheerio';
 import moment from 'moment'
 
 export const parse = async () => {
@@ -6,14 +7,31 @@ export const parse = async () => {
     var path = `${process.cwd()}/src/data/`
     var today = ['mon','tue','wed','thu','fri','sat'][moment().day()];
     var filename = `${today}.md`
+    var list = [
+        '- 미분당',
+        '- 닭한마리',
+        '- 낙지한마리',
+        '- 빨간집',
+        '- 감성타코',
+        '- 도산분식',
+        '- 샐러디',
+    ];
+    let i = 0;
+    let result = [];
+    while (i < 3) {
+        result = result.concat(list.splice(Math.floor(Math.random() * list.length), 1));
+        i++;
+    }
+
+    console.log(result);
 
     // console.log(path)
     // console.log(today)
     // console.log(filename)
 
     try {
-        var menu = fs.readFileSync(path + `${filename}`);
-        return menu.toString()
+        // var menu = fs.readFileSync(path + `${filename}`);
+        return result.join('\n');
     } catch(err) {
         if (err.code === 'ENOENT') {
             console.log('File not found!');
@@ -23,3 +41,4 @@ export const parse = async () => {
     
     
 }
+console.log(parse());
